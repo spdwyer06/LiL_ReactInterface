d8881a65ca08ac008e67234cc262ef7d35dd21ad
Sean Dwyer <spdwyer06@gmail.com>
2021-04-20 10:44:04 -0400

    Add seed data file in public folder. Add useEffect to pull in seed data and set state variable

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import {useState, useEffect} from 'react';
+
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments';
@@ -5,12 +7,32 @@ import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
 function App() {
+
+  // const [myName, setMyName] = useState('Sean');
+  const [myAppointments, setMyAppointments] = useState([]);
+
+  useEffect(() => {
+    fetchSeedData();
+  }, []);
+  
+  const fetchSeedData = async() => {
+    const res = await fetch('./seedData.json');
+    const result = await res.json();
+    const appointments = result.map(item => {
+      return item;
+    });
+    setMyAppointments(appointments);
+  }
+
+  
+
   return (
     <main className="page bg-white" id="petratings">
     <div className="container">
       <div className="row">
         <div className="col-md-12 bg-white">
           <div className="container">
+            {/* {myName} */}
             <AddAppointments />
             <SearchAppointments />
             <ListAppointments />
